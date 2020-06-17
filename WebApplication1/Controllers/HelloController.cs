31d28d33870aaac4be8b43654065cbf50e795399
Lunaro0 <846812423@qq.com>
2020-06-17 15:51:49 +0800

    111

---
+++ WebApplication1/Controllers/HelloController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class HelloController : Controller
+    {
+        // GET: Hello
+        public ActionResult Index(string name="hhh",int num=1)
+        {
+            ViewBag.m = "hello" + name;
+            ViewBag.num = num;
+            return View();
+        }
+    }
+}
